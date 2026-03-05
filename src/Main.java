36542600d1557e9aa9fd6ba63638990a03940f55
ivan <ivanbrtprz@gmail.com>
2026-03-05 00:36:30 +0000

    Feature metodo mostrarTarea y opcion 2 del menu

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        GestorTareas gestor = new GestorTareas();
+
+        int opcion;
+        do {
+            System.out.println("1. Añadir tarea");
+            System.out.println("2. Listar tareas");
+            System.out.println("0. Salir");
+
+            opcion = sc.nextInt();
+            sc.nextLine();
+
+            switch (opcion) {
+                case 2:
+                    gestor.mostrarTareas();
+                    break;
+            }
+        } while (opcion != 0);
+    }
+}
